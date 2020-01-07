67b497d747839bcb0f03a31380fe6d6ac31ed5e1
Alastair <alastair>
2020-01-07 19:54:02 +0000

    Fix issues caused by running with sudo.

--- quickinstall.sh
+++ quickinstall.sh
@@ -2,36 +2,32 @@
 
 # Quickly install my frequently used software
 
-if [[ $EUID -ne 0 ]]; then
-   echo "Use sudo to run this script. Type sudo ./quickinstall.sh" 
-   exit 1
-fi
-
 echo "Updating Repository List"
-apt-get update -y -q
+sudo apt-get update -y -q
 
 echo "Installing default GNOME"
-apt-get install -y -q gnome-session
+sudo apt-get install -y -q gnome-session
 
 echo "Using the default GNOME GDM"
-update-alternatives --set gdm3.css /usr/share/gnome-shell/theme/gnome-shell.css 
+sudo update-alternatives --set gdm3.css /usr/share/gnome-shell/theme/gnome-shell.css 
 
 echo "Removing the Amazon launcher"
-apt purge -y ubuntu-web-launchers 
+sudo apt purge -y ubuntu-web-launchers 
 
 echo "Installing CLion"
-snap install --classic clion
+sudo snap install --classic clion
 
 echo "Installing Spotify"
-snap install spotify
+sudo snap install spotify
 
 echo "Installing Steam"
-apt-get install -y -q steam-installer
+sudo apt-get install -y -q steam-installer
 
 echo "Installing VLC Media Player"
-apt-get install -y -q vlc
+sudo apt-get install -y -q vlc
 
 echo "Applying a nice wallpaper"
+# Without sudo so we get the user's home folder and the file isn't owned by root.
 wget https://github.com/atoft/wallpaper/raw/master/wallpaper.png -P ~/Pictures
 gsettings set org.gnome.desktop.background picture-uri "file:///$HOME/Pictures/wallpaper.png"
 
