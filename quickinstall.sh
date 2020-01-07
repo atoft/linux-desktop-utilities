a9d0b9abfe40382c5ad5be1e2c66eb8eb4e987d1
Alastair <alastair>
2020-01-07 21:01:07 +0000

    Fix calculator integration.

--- quickinstall.sh
+++ quickinstall.sh
@@ -26,6 +26,10 @@ sudo apt-get install -y -q steam-installer
 echo "Installing VLC Media Player"
 sudo apt-get install -y -q vlc
 
+echo "Fixing Calculator integration"
+snap remove gnome-calculator
+sudo apt-get install -y -q gnome-calculator
+
 echo "Applying a nice wallpaper"
 # Without sudo so we get the user's home folder and the file isn't owned by root.
 wget https://github.com/atoft/wallpaper/raw/master/wallpaper.png -P ~/Pictures
