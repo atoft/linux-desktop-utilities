5afabdcbe8329b8800b3662e7b56f1671229ccdf
Alastair <me@atoft.dev>
2020-11-01 11:33:05 +0000

    Tweak emacs visuals. Ensure packages are installed.

--- emacs/atoft-config.el
+++ emacs/atoft-config.el
@@ -20,29 +20,63 @@
 (package-refresh-contents)
 (package-install 'use-package))
 
-;; Solarized
-(load-theme 'solarized-dark t)
+;; Themes and visuals
+(use-package solarized-theme
+  :ensure t
+  :config
+  (load-theme 'solarized-dark t))
+
+(setq solarized-distinct-fringe-background t)
+(setq solarized-high-contrast-mode-line t)
+
+(use-package all-the-icons
+  :ensure t)
+
+(use-package doom-modeline
+  :ensure t
+  :hook (after-init . doom-modeline-mode))
+
+(set-face-attribute 'mode-line nil
+                    :overline nil
+                    :underline nil)
+
+(set-face-attribute 'mode-line-inactive nil
+                    :overline nil
+                    :underline nil)
+
+(global-hl-line-mode t)
 
 ;; Tree view
-(use-package treemacs)
+(use-package treemacs
+  :ensure t)
+(use-package treemacs-all-the-icons
+  :ensure t
+  :config
+  (treemacs-load-theme 'all-the-icons))
 
 ;; Projects
-(use-package projectile)
+(use-package projectile
+  :ensure t)
 (projectile-mode +1)
 (define-key projectile-mode-map (kbd "M-p") 'projectile-command-map)
 
-(use-package treemacs-projectile)
+(use-package treemacs-projectile
+  :ensure t)
 
 ;; Company mode
-(use-package company)
+(use-package company
+  :ensure t)
 
 ;; C++
 (setq c-default-style "bsd")
 (setq-default c-basic-offset 4)
-(add-hook 'c++-mode-hook #'display-line-numbers-mode)
+(add-hook 'prog-mode-hook #'display-line-numbers-mode)
 
 
 ;; LSP mode:
+(use-package lsp-mode
+  :ensure t)
+
 (setq lsp-keymap-prefix "C-l")
 
 ;; Support cross-file rename for C++ in LSP.
@@ -57,7 +91,9 @@
 ;;  (yas-global-mode))
 
 ;; optionally
-(use-package lsp-ui :commands lsp-ui-mode)
+(use-package lsp-ui
+  :ensure t
+  :commands lsp-ui-mode)
 ;; if you are helm user
 ;;(use-package helm-lsp :commands helm-lsp-workspace-symbol)
 ;; if you are ivy user
@@ -81,6 +117,7 @@
 
 ;; Hide the toolbar
 (tool-bar-mode -1)
+(scroll-bar-mode -1)
 
 ;; Refresh buffer if file changes on disk
 (global-auto-revert-mode t)
