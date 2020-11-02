6bba4ebff86b7cb4d71e798a78f4fb06fea0052c
atoft <me@atoft.dev>
2020-11-02 19:37:18 +0000

    Remove custom variables.

--- emacs/atoft-config.el
+++ emacs/atoft-config.el
@@ -135,21 +135,3 @@
 ;; Visual line wrapping
 (add-hook 'org-mode-hook #'visual-line-mode)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("c433c87bd4b64b8ba9890e8ed64597ea0f8eb0396f4c9a9e01bd20a04d15d358" default)))
- '(org-agenda-files (quote ("~/Sync/Documents/todo.org")))
- '(package-selected-packages
-   (quote
-    (treemacs-projectile use-package unicode-fonts treemacs solarized-theme projectile lsp-ui flycheck))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
